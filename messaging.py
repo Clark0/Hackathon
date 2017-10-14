5118e74c61d2037ca84649087b0d3b893105dd0e
CL <caoliu98@yahoo.com>
2017-10-14 20:52:29 +0800

    This
    testcase, sendMessage,

--- messaging.py
+++ messaging.py
@@ -1,13 +1,16 @@
 import time
 from socket import *
+from collections import deque
 
 class messaging:
 	def __init__(self, myPubKey):
 		self.table = {}
 		self.myPubKey = myPubKey
+		self.messageQueue = deque()
 		self.broadCasting = socket(AF_INET, SOCK_DGRAM)
 		self.broadCasting.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 		self.broadCasting.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+		self.myPort = 54542
 
 
 	def iplookup(self, targetPubKey):
@@ -22,15 +25,28 @@ class messaging:
 		except KeyError:
 			self.ask(targetPubKey)
 
-	def ask(self, targetPubKey):
-
-
+	# def ask(self, targetPubKey):
 
 
 	def getMyIP(self):
 		s = socket(AF_INET, SOCK_DGRAM)
 		s.connet(("8.8.8.8", 80))
 		return s.getsockname()[0]
+
+	def dequeMessage(self):
+		item = messageQueue.get(block=True, timeout=None)
+		if item is not None:
+			return item
+
+	def sendMessage(self, IP, message):
+		myIP = self.getMyIP()
+		s = socket(AF_INET, socket.SOCK_STREAM)
+		s.connet(IP, self.myPort)
+		s.send(IP, message.decode)
+		s.close()
+
+
+
 
 
 
