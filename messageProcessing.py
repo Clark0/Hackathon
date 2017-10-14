5034c65e19ca862b5e2ac11fa932678685e12147
Qiu Haoze <qiuh0008@e.ntu.edu.sg>
2017-10-15 03:15:19 +0800

    Corrected time-sent

--- messageProcessing.py
+++ messageProcessing.py
@@ -1,5 +1,6 @@
 import re
 import time
+import datetime
 
 def messagePacking(myPubKey,recipientPubKey,message):
 	packedMsg = '''From: {}\r\nTo: {}\r\nMessage-Content: {}\r\nTime-Sent: {}\r\n'''.format(myPubKey,recipientPubKey,message,time.time())
@@ -11,6 +12,6 @@ def messageUnpacking(packedMsg):
 	message['from'] = re.search(r"From: .*?\r\n",decoded).group().strip('From: ').strip('\r\n')
 	message['to'] = re.search(r"To: .*?\r\n",decoded).group().strip('To: ').strip('\r\n')
 	message['content'] = re.search(r"Message-Content: .*?\r\n",decoded).group().strip('Message-Content: ').strip('\r\n')
-	message['time-sent'] = re.search(r"Time-Sent: .*?\r\n",decoded).group().strip('Time-Sent: ').strip('\r\n')
+	message['time-sent'] = datetime.datetime.fromtimestamp(round(float(re.search(r"Time-Sent: .*?\r\n",decoded).group().strip('Time-Sent: ').strip('\r\n')))).utcnow().strftime("%a, %d %b %Y %X %z")
 	return message
 
